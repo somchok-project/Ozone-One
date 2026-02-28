91e92bd2ec63891a29f31ea501f2ed0653dd4df1
Phatitee <chaimongcol08@gmail.com>
2026-02-28 16:47:56 +0700

    feat: register booth and booking routers in tRPC root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,6 @@
 import { postRouter } from "@/server/api/routers/post";
+import { boothRouter } from "@/server/api/routers/booth";
+import { bookingRouter } from "@/server/api/routers/booking";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -8,6 +10,8 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  booth: boothRouter,
+  booking: bookingRouter,
 });
 
 // export type definition of API
